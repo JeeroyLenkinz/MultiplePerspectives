cae2adf509f5676597b9a42fed919b0f8eedea37
JeeroyLenkinz <rdotsikas@gmail.com>
2018-09-10 12:22:22 -0400

    Movement Finished
    Can move vertical along track and use A/D to track change left/right

---
+++ MultiplePerspectives_Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private bool isRotating;
+    private int trackDirection;
+    private float totalAmountRotated;
+    private bool canSetZero;
+
+    public float playerSpeed;
+    public float trackChangeSpeed;
+    public float circleRadius;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        isRotating = false;
+        totalAmountRotated = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+        //Listen for key press of the track change buttons
+        if (Input.GetKeyDown("a") && !isRotating)
+        {
+            isRotating = true;
+            trackDirection = 1; //Move left (CW)
+        }
+        else if (Input.GetKeyDown("d") && !isRotating) {
+            isRotating = true;
+            trackDirection = -1; //Move right (CCW)
+        }
+        if (isRotating)
+        {
+            TrackChange(trackDirection); //if isRotating has been set to true run the TrackChange function every frame until finished
+        }
+	}
+
+    private void Move()
+    {
+        if (!isRotating)
+        {
+            float moveVertical = Input.GetAxis("Vertical");
+            float movement = moveVertical * playerSpeed;
+
+            rb.position = new Vector3(rb.position.x, rb.position.y + movement, rb.position.z); //X and Z positions untouched, can only move in the Y direction
+        }
+    }
+
+    private void TrackChange(int direction)
+    {
+        canSetZero = false;
+        float rotateAmount = Time.deltaTime * trackChangeSpeed * direction; //Amount to be rotated on each call of Update, takes direction into account
+
+        if (Mathf.Abs(90.0f - Mathf.Abs(totalAmountRotated)) <= Mathf.Abs(rotateAmount)) //Check if the amount you are about to rotate puts you past 90 degrees of total rotation
+        {
+            rotateAmount = (90.0f - Mathf.Abs(totalAmountRotated))*direction; //Set the amount to be rotated to the remainder before reaching 90 degrees
+            isRotating = false;
+            canSetZero = true; //totalAmountRotated needs to be incremented after the RotateAround so we need to know to set it to zero afterwards
+        }
+
+        rb.transform.RotateAround(Vector3.zero, Vector3.up, rotateAmount); //Rotate around the point 0,0,0 and Y axis by the rotateAmount degrees
+        totalAmountRotated += rotateAmount; //Update how many degrees we've now rotated in total
+
+        if (canSetZero)
+        {
+            totalAmountRotated = 0;
+        }
+    }
+
+}
